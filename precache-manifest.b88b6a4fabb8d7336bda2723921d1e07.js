f38950c49eaf19fde32c8e13b13abefa64f909ad
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2021-02-01 13:43:23 +0530

    Updates

---
+++ precache-manifest.b88b6a4fabb8d7336bda2723921d1e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fe161eb9b8ee84c62821206b7cb91f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28456587a75320d37815",
+    "url": "./static/css/main.3a9a7ba5.chunk.css"
+  },
+  {
+    "revision": "a3d96122c0be60cec3d6",
+    "url": "./static/js/2.f6724d0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f6724d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28456587a75320d37815",
+    "url": "./static/js/main.0d1e5446.chunk.js"
+  },
+  {
+    "revision": "f7a0d689a3fb70786813",
+    "url": "./static/js/runtime-main.ef3808fc.js"
+  }
+]);
