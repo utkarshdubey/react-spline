39703a3df4299b35be945aae14a13b5b05003c3d
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2021-07-03 02:21:54 +0530

    Possible Patch 3

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import { SPLINE_EXPORTED_SCENE_TWO } from './scene2'
-import SPLINE_EXPORTED_SCENE from './scene.json'
+// import SPLINE_EXPORTED_SCENE from './scene.json'
+import SPLINE_EXPORTED_SCENE_TWO from './scene-two.json'
 import { Spline } from 'react-spline'
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
         <h1 style={{ position: 'absolute', zIndex: 1, color: '#fff' }}>
           If you see this, react-spline works.
         </h1>
-        <Spline scene={SPLINE_EXPORTED_SCENE} />
+        <Spline scene={SPLINE_EXPORTED_SCENE_TWO} />
       </div>
       {/* <div
         style={{
